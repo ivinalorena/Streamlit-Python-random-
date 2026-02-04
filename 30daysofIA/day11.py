a5d4b9c3959272d149f4b324ff2b7508c0d72824
Ivina Lorena Oliveira Moura <ivinalorena@aluno.unilab.edu.br>
2026-02-03 22:02:57 -0300

    day 11 nao finalizado

---
+++ 30daysofIA/day11.py
@@ -0,0 +1,63 @@
+""" Day 11
+Displaying Chat History
+For today's challenge, our goal is to enhance our chatbot with better history management. We need to add a welcome message, '
+'display conversation statistics in the sidebar, and provide a way to clear the chat history. Once that's done, we will have 
+a more polished chatbot experience with visible conversation tracking.
+
+Note: We also add st.rerun() after the assistant's response to ensure the sidebar stats update immediately. """
+import streamlit as st
+import streamlit as st
+import json
+from snowflake.snowpark.functions import ai_complete
+
+try:
+    from snowflake.snowpark.context import get_active_session
+    session = get_active_session()
+except:
+    from snowflake.snowpark import Session
+    session = Session.builder.configs(st.secrets["connections"]["snowflake"]).create()
+
+def call_llm(prompt_text:str)->str:
+    df=session.range(1).select(
+         ai_complete(model="claude-3-5-sonnet", 
+        prompt=prompt_text).alias("response")
+    )
+    response_raw = df.collect()[0][0]
+    response_json = json.loads(response_raw)
+
+    if isinstance(response_json, dict):
+        return response_json.get("choices", [{}])[0].get("messages","")
+    return str(response_json)
+
+st.title(":material/chat: Chatbot com histórico")
+
+
+if "messages" not in st.session_state:
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Olá, eu sou sua IA assistente. Como eu posso ajudar você hoje?"}
+    ]
+
+with st.sidebar:
+    st.header("Conversation Stats")
+    user_msgs = len([m for m in st.session_state.messages if m["role"]== "user"])
+    assistant_msgs = len([m for m in st.session_state.messages if m["role"]== "assistant"])
+""" 
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message("content"))
+
+if prompt := st.chat_input("O que você gostaria de saber?"):
+    st.session_state.messages.append({"role":"user", "content":prompt})
+
+    with st.chat_message("user"):
+        st.write(prompt)
+
+    with st.chat_message("assistant"):
+        response = call_llm(prompt)
+        st.write(response)
+
+    st.session_state.messages.append({"role":"assistant","content": response})
+ """
+    
+st.divider()
+st.caption("Day 11: Displaying Chat History | 30 Days of AI")
